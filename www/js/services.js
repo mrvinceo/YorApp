8766f58fe072c1cfd9fa8affc69ecb85448eaa03
Paul Vincent <paulvincent@oc.ac.uk>
2016-10-06 11:24:53 +0100

    Added Darksky API call to services.js for Feature

--- www/js/services.js
+++ www/js/services.js
@@ -48,3 +48,53 @@ angular.module('starter.services', [])
     }
   };
 });
+
+//Weather API function using DarkSky API.
+(function () {
+  'use strict';
+  var apiKey = "a7acef80cf3c70bffc4d8d4107394a46";
+  var forecastDarkSky = ['$q', '$resource', '$http', 'apiKey',
+    function($q, $resource, $http, apiKey) {
+    var url = 'https://api.darksky.net/forecast/' + apiKey + '/';
+
+    var weatherResource = $resource(url, {
+      callback: 'JSON_CALLBACK',
+    }, {
+      get: {
+        method: 'JSONP'
+      }
+    });
+
+    return {
+      //getAtLocation: function(lat, lng) {
+      getCurrentWeather: function(lat, lng) {
+        return $http.jsonp(url + lat + ',' + lng + '?callback=JSON_CALLBACK&units=si');
+      }
+    };
+  }];
+
+  angular.module('starter.services', ['ngResource'])
+
+  .factory('DataStore', function() {
+      //create datastore with default values
+      var DataStore = {
+          city:       'Portumna',
+          latitude:   53.0905,
+          longitude:  -8.2337 };
+
+      DataStore.setCity = function (value) {
+         DataStore.city = value;
+      };
+
+      DataStore.setLatitude = function (value) {
+         DataStore.longitude = value;
+      };
+
+      DataStore.setLongitude = function (value) {
+         DataStore.longitude = value;
+      };
+
+      return DataStore;
+  })
+  .factory('Weather', forecastDarkSky);
+}());
